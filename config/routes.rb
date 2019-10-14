3f1912e1478037fda69e54fd9c20c26cf2ca26d6
Dev Teknuk <dev@teknuk.com>
2019-10-14 17:06:39 +0500

    Added portfolio items via Resource generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :guides
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :portfolios
   get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
