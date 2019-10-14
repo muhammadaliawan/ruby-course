6348446f9ee00f75df9144657b25a781ebd1ad2b
Dev Teknuk <dev@teknuk.com>
2019-10-14 16:15:49 +0500

    Built pages via controller generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :guides
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'pages/home'
+  get 'pages/about'
+  get 'pages/contact'
+  resources :guides
 end
