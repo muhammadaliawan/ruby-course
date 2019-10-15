c241b9960502df50046c9131cebb780a3c3d69a7
Dev Teknuk <dev@teknuk.com>
2019-10-15 14:46:35 +0500

    Integrated friendly routes for guides post

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -64,7 +64,7 @@ class GuidesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_guide
-      @guide = Guide.find(params[:id])
+      @guide = Guide.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
