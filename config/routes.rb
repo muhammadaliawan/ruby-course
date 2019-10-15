ed32f112381c61caf79353a8ec609439a6d936d7
Dev Teknuk <dev@teknuk.com>
2019-10-15 13:54:23 +0500

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :guides
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :portfolios
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show',as: 'portfolio_show'
 
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
