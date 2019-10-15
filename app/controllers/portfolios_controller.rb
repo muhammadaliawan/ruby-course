bea8f1f1e50e681ed26a57e0daca022886bc325a
Dev Teknuk <dev@teknuk.com>
2019-10-15 13:22:02 +0500

    Implemented destroy action for portfolio feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -41,6 +41,20 @@ def show
 	@portfolio_item = Portfolio.find(params[:id])
 end
 
+
+def destroy
+	# Perform the lookup
+	@portfolio_item = Portfolio.find(params[:id])
+
+	# Destroy/delete the recoord
+	@portfolio_item.destroy
+
+	# Redirect
+	respond_to do |format|
+		format.html {redirect_to portfolios_url, notice: 'Record was removed.'}
+	end
+end	
+
 def portfolio_item_params
 	params.require(:portfolio).permit(:title, :subtitle, :boby)
 end
