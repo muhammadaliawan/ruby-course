dae2a439972aed99baabbe6881b79d24892d23d6
Dev Teknuk <dev@teknuk.com>
2019-10-15 11:35:31 +0500

    Integrated new and create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,8 +1,27 @@
 class PortfoliosController < ApplicationController
 
 def index
-	@portfolio_items = Portfolio.all
+	@portfolio_item = Portfolio.all
 
 end
 
+def new
+	@portfolio_item = Portfolio.new
+end	
+
+def create
+	@portfolio_item = Portfolio.new(portfolio_item_params)
+
+	respond_to do |format|
+		if @portfolio_item.save
+			format.html {redirect_to portfolios_path, notice: 'Your portfolio item is now live.'}
+		else
+			format.html {render :new}
+		end
+	end
+end
+
+def portfolio_item_params
+	params.require(:portfolio).permit(:title, :subtitle, :boby)
+end
 end
