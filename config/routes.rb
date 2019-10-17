1b2cd7f3f926d5bed70c4cb3478d50b8b907ce5d
Dev Teknuk <dev@teknuk.com>
2019-10-17 18:05:31 +0500

    Implemented basic authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :guides
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
   resources :portfolios, except: [:show]
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show',as: 'portfolio_show'
