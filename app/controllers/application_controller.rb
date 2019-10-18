24b35cbe0074b1f41be9d5a0132d33d7be843c16
Dev Teknuk <dev@teknuk.com>
2019-10-18 13:05:58 +0500

    Added controller concerns for devise params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  include DeviseWhitelist
+
   protect_from_forgery with: :exception
 
   before_filter :configure_permitted_parameters, if :devise_controller?
