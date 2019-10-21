3cf934425aa171e50c5b13de4fa9847a2e4a54fd
Dev Teknuk <dev@teknuk.com>
2019-10-21 16:05:56 +0500

    Added custom layouts for guides and portfolios

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,8 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
+# Rails.application.config.assets.precompile += %w( admin.js )
+Rails.application.config.assets.precompile += %w(guides.scss)
+Rails.application.config.assets.precompile += %w(portfolios.scss)
