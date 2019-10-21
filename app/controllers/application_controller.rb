3766925306217f7ace548f751515d79a33d68890
Dev Teknuk <dev@teknuk.com>
2019-10-21 15:19:53 +0500

    Added nav bar and added default header content

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   include DeviseWhitelist
   include SetSource
   include CurrentUserConcern
+  include DefaultPageContent
 
   protect_from_forgery with: :exception
 
