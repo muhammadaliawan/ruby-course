74a24abee876de89b8d1e9ad79e3cc6c0993af54
Dev Teknuk <dev@teknuk.com>
2019-10-21 12:00:06 +0500

    Refactored strong params for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -16,7 +16,7 @@ def new
 end	
 
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+    @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
@@ -35,7 +35,7 @@ end
     @portfolio_item = Portfolio.find(params[:id])
 
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+      if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
       else
         format.html { render :edit }
@@ -61,7 +61,12 @@ def destroy
 	end
 end	
 
-def portfolio_item_params
-	params.require(:portfolio).permit(:title, :subtitle, :boby)
+private def portfolio_params
+	params.require(:portfolio).permit(:title, 
+                                    :subtitle, 
+                                    :body, 
+                                    technologies_attributes: [:name]
+                                    )
 end
+
 end
