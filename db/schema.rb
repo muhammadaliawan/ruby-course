94c592698c5affb058dbd1ab7b8afb69a2f3f9e6
Dev Teknuk <dev@teknuk.com>
2019-10-22 16:58:58 +0500

    Added installation requirements for petergate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191017125509) do
+ActiveRecord::Schema.define(version: 2019_10_22_115526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 20191017125509) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "slug"
     t.integer "status", default: 0
-    t.integer "topic_id"
+    t.bigint "topic_id"
     t.index ["slug"], name: "index_guides_on_slug", unique: true
     t.index ["topic_id"], name: "index_guides_on_topic_id"
   end
@@ -94,6 +94,13 @@ ActiveRecord::Schema.define(version: 20191017125509) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "roles"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "guides", "topics"
