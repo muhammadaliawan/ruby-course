8f044d0ccb34dc83a65dc34b011e4619f40bf4cd
Dev Teknuk <dev@teknuk.com>
2019-10-22 16:27:07 +0500

    Integrated new gem into rails app for copyright

--- Gemfile
+++ Gemfile
@@ -49,3 +49,5 @@ gem 'friendly_id', '~> 5.1.0'
 gem 'devise', '~> 4.7', '>= 4.7.1'
 
 gem 'bootstrap', '~> 4.3.1'
+
+gem 'teknuk_view_tool', git:"https://github.com/muhammadaliawan/teknuk_view_tool"
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
     		content_tag(:p, greeting, class: "source-greeting")
     	end
 	end
+
+  def copyright_generator
+    TeknukViewTool::Renderer.copyright 'TekNuk', 'All rights reserved'
+  end
 end
