b4dcbc46e073a4d886b610cff9909a2b3b580c64
Dev Teknuk <dev@teknuk.com>
2019-10-28 12:27:14 +0500

    Integrated initial drag and drop implementation and some minor updates other files

--- db/migrate/20191024122438_add_position_to_portfolios.rb
+++ db/migrate/20191024122438_add_position_to_portfolios.rb
@@ -1,4 +1,4 @@
-class AddPositionToPortfolios < ActiveRecord::Migration[6.0]
+class AddPositionToPortfolios < ActiveRecord::Migration[5.0]
   def change
     add_column :portfolios, :position, :integer
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_24_122438) do
+ActiveRecord::Schema.define(version: 20191024122438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,14 +36,6 @@ ActiveRecord::Schema.define(version: 2019_10_24_122438) do
     t.integer  "topic_id"
     t.index ["slug"], name: "index_guides_on_slug", unique: true, using: :btree
     t.index ["topic_id"], name: "index_guides_on_topic_id", using: :btree
-    t.string "title"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "slug"
-    t.integer "status", default: 0
-    t.bigint "topic_id"
-    t.index ["slug"], name: "index_guides_on_slug", unique: true
-    t.index ["topic_id"], name: "index_guides_on_topic_id"
   end
 
   create_table "portfolios", force: :cascade do |t|
@@ -54,14 +46,7 @@ ActiveRecord::Schema.define(version: 2019_10_24_122438) do
     t.text     "thumb_image"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.string "title"
-    t.string "subtitle"
-    t.text "body"
-    t.text "main_image"
-    t.text "thumb_image"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "position"
+    t.integer  "position"
   end
 
   create_table "skills", force: :cascade do |t|
@@ -100,15 +85,9 @@ ActiveRecord::Schema.define(version: 2019_10_24_122438) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "roles"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-    t.inet "current_sign_in_ip"
-    t.inet "last_sign_in_ip"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "roles"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "guides", "topics"
