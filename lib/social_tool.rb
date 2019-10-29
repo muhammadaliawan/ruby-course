be1121ed18db027240b4152541b82bf651a7f4b3
Dev Teknuk <dev@teknuk.com>
2019-10-29 12:25:09 +0500

    Implemented initial connection to Twitter API

---
+++ lib/social_tool.rb
@@ -0,0 +1,20 @@
+module SocialTool
+	def self.twitter_search
+		client = Twiter::REST::Client.new do |config|
+			config.consumer_key			= ENV.fetch("TWITTER_CONSUMER_KEY")
+			config.consumer_secret 		= ENV.fetch("TWITTER_CONSUMER_SECRET")
+			config.access_token 		= ENV.fetch("TWITTER_ACCESS_TOKEN")
+			config.access_token_secret	= ENV.fetch("TWITTER_ACCESS_SECRET")
+		end
+
+		client.search("#rails", result_type: 'recent').take(6).collect do |tweet|
+			"#{tweet.user.screen_name}: #{tweet.text}" 
+		end
+	end
+end
+
+# Place these keys in env file and uncomment.
+# TWITTER_CONSUMER_KEY=
+# TWITTER_CONSUMER_SECRET=
+# TWITTER_ACCESS_TOKEN=
+# TWITTER_ACCESS_SECRET=
