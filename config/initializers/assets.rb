5c16c4c218287564813d206562d9601c85eeccba
Dev Teknuk <dev@teknuk.com>
2019-10-30 15:42:31 +0500

    Integrated custom fonts

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,3 +14,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( admin.js )
 Rails.application.config.assets.precompile += %w(guides.scss)
 Rails.application.config.assets.precompile += %w(portfolios.scss)
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
